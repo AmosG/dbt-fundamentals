622b6b08709d273a596d117789cd0699170bbd90
Amos Gutman <amos.gutman@gmail.com>
2022-04-03 11:28:38 +0300

    added ref functions

--- models/example/dim_customers.sql
+++ models/example/dim_customers.sql
@@ -1,23 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from amos_dbt.customers
+    select * from {{ ref('stg_customers')}}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from amos_dbt.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
